978d629ba5705bd7612d27b74cb016f955acb29d
zhaofeng <zhaofeng@kuaishou.com>
2021-07-01 18:19:37 +0800

    update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,10 @@
 <template>
-    <el-container>
-        <el-button type='info' @click="logout">退出</el-button>
-        <el-header></el-header>
+    <el-container class="home-container">
+        <el-header>
+            <img src="../assets/heima.png" alt="">
+            <span>电商管理后台</span>
+            <el-button type='info' @click="logout">退出</el-button>
+        </el-header>
           <el-container>
             <el-aside width="200px">Aside</el-aside>
             <el-main>Main</el-main>
@@ -19,5 +22,26 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+    .home-container{
+      height: 100%;
+    }
+    .el-header{
+      background-color: #383d41;
+      display: flex;
+      justify-content: space-between;
+      padding-left: 0;
+      align-items: center;
+      color: #fff;
+      font-size: 20px;
+
+    }
+
+    .el-aside{
+        background-color: #343743;
+    }
+
+    .el-main{
+        background-color: #eaedf1;
+    }
 
 </style>
