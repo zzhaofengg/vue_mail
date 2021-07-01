05d67e0b04a1a32ffcc334cd0dbd867693b92d03
zhaofeng <zhaofeng@kuaishou.com>
2021-07-01 17:54:36 +0800

    hei

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,12 @@
 <template>
-    <div>
-      <el-button type='info' @click="logout">退出</el-button>
-    </div>
+    <el-container>
+        <el-button type='info' @click="logout">退出</el-button>
+        <el-header></el-header>
+          <el-container>
+            <el-aside width="200px">Aside</el-aside>
+            <el-main>Main</el-main>
+          </el-container>
+        </el-container>
 </template>
 <script>
 export default {
