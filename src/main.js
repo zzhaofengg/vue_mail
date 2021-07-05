3dfc0f6a9c61b58e8b65c42b5065cc4cfbc58282
zhaofeng <zhaofeng@kuaishou.com>
2021-07-05 20:01:27 +0800

    cate update

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import './plugins/element.js'
 import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 import axios from 'axios'
+
+import TreeTable from 'vue-table-with-tree-grid'
 // 配置请求根路径
 axios.defaults.baseURL = 'http://zzhaofengg.asia:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
@@ -14,6 +16,7 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Welcome from '../components/Welcome.vue'
 import Users from '../components/user/Users.vue'
 import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
+import Cate from '../components/goods/Cate.vue'
 
 Vue.use(VueRouter)
 const routes = [{
@@ -32,6 +33,8 @@ const routes = [{
   }, {
     path: '/roles',
     component: Roles
+  }, {
+    path: '/categories', component: Cate
   }]
 }
 ]
