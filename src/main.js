c94c5c464f0fa2e299d9ec654c43cd21d48719c3
zhaofeng <zhaofeng@kuaishou.com>
2021-07-06 19:47:08 +0800

    add update

--- src/main.js
+++ src/main.js
@@ -7,6 +7,13 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 
 import TreeTable from 'vue-table-with-tree-grid'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// require styles 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 // 配置请求根路径
 axios.defaults.baseURL = 'http://zzhaofengg.asia:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
@@ -17,6 +24,22 @@ Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 Vue.component('tree-table', TreeTable)
+// 将富文本编辑器，注册为全局可用的组件
+Vue.use(VueQuillEditor)
+
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
