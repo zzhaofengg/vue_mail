8ff57ac9e33cc9c4d442e5f1c3ba8d177a5a01fd
zhaofeng <zzhaofengg@yeah.net>
2021-07-06 22:14:38 +0800

    Add done

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -1,86 +1,86 @@
 <template>
-    <div>
-      <!-- 面包屑导航区域 -->
-      <el-breadcrumb separator-class="el-icon-arrow-right">
-        <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item>商品管理</el-breadcrumb-item>
-        <el-breadcrumb-item>添加商品</el-breadcrumb-item>
-      </el-breadcrumb>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>添加商品</el-breadcrumb-item>
+    </el-breadcrumb>
 
-      <!-- 卡片视图 -->
-      <el-card>
-        <!-- 提示区域 -->
-        <el-alert title="添加商品信息" type="info" center show-icon :closable="false">
-        </el-alert>
-        <!-- 步骤条区域 -->
-        <el-steps :space="200" :active="activeIndex - 0" finish-status="success" align-center>
-          <el-step title="基本信息"></el-step>
-          <el-step title="商品参数"></el-step>
-          <el-step title="商品属性"></el-step>
-          <el-step title="商品图片"></el-step>
-          <el-step title="商品内容"></el-step>
-          <el-step title="完成"></el-step>
-        </el-steps>
+    <!-- 卡片视图 -->
+    <el-card>
+      <!-- 提示区域 -->
+      <el-alert title="添加商品信息" type="info" center show-icon :closable="false">
+      </el-alert>
+      <!-- 步骤条区域 -->
+      <el-steps :space="200" :active="activeIndex - 0" finish-status="success" align-center>
+        <el-step title="基本信息"></el-step>
+        <el-step title="商品参数"></el-step>
+        <el-step title="商品属性"></el-step>
+        <el-step title="商品图片"></el-step>
+        <el-step title="商品内容"></el-step>
+        <el-step title="完成"></el-step>
+      </el-steps>
 
-        <!-- tab栏区域 -->
+      <!-- tab栏区域 -->
 
-        <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="100px" label-position="top">
-          <el-tabs v-model="activeIndex" :tab-position="'left'" :before-leave="beforeTabLeave" @tab-click="tabClicked">
-            <el-tab-pane label="基本信息" name="0">
-              <el-form-item label="商品名称" prop="goods_name">
-                <el-input v-model="addForm.goods_name"></el-input>
-              </el-form-item>
-              <el-form-item label="商品价格" prop="goods_price">
-                <el-input v-model="addForm.goods_price" type="number"></el-input>
-              </el-form-item>
-              <el-form-item label="商品重量" prop="goods_weight">
-                <el-input v-model="addForm.goods_weight" type="number"></el-input>
-              </el-form-item>
-              <el-form-item label="商品数量" prop="goods_number">
-                <el-input v-model="addForm.goods_number" type="number"></el-input>
-              </el-form-item>
-              <el-form-item label="商品分类" prop="goods_cat">
-                <el-cascader expand-trigger="hover" :options="catelist" :props="cateProps" v-model="addForm.goods_cat" @change="handleChange">
-                </el-cascader>
-              </el-form-item>
-            </el-tab-pane>
-            <el-tab-pane label="商品参数" name="1">
-              <!-- 渲染表单的Item项 -->
-              <el-form-item :label="item.attr_name" v-for="item in manyTableData" :key="item.attr_id">
-                <!-- 复选框组 -->
-                <el-checkbox-group v-model="item.attr_vals">
-                  <el-checkbox :label="cb" v-for="(cb, i) in item.attr_vals" :key="i" border></el-checkbox>
-                </el-checkbox-group>
-              </el-form-item>
-            </el-tab-pane>
-            <el-tab-pane label="商品属性" name="2">
-              <el-form-item :label="item.attr_name" v-for="item in onlyTableData" :key="item.attr_id">
-                <el-input v-model="item.attr_vals"></el-input>
-              </el-form-item>
-            </el-tab-pane>
-            <el-tab-pane label="商品图片" name="3">
-              <!-- action 表示图片要上传到的后台API地址 -->
-              <el-upload :action="uploadURL" :on-preview="handlePreview" :on-remove="handleRemove" list-type="picture" :headers="headerObj" :on-success="handleSuccess">
-                <el-button size="small" type="primary">点击上传</el-button>
-              </el-upload>
-            </el-tab-pane>
-            <el-tab-pane label="商品内容" name="4">
-              <!-- 富文本编辑器组件 -->
-              <quill-editor v-model="addForm.goods_introduce"></quill-editor>
-              <!-- 添加商品的按钮 -->
-              <el-button type="primary" class="btnAdd" @click="add">添加商品</el-button>
-            </el-tab-pane>
-          </el-tabs>
-        </el-form>
+      <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="100px" label-position="top">
+        <el-tabs v-model="activeIndex" :tab-position="'left'" :before-leave="beforeTabLeave" @tab-click="tabClicked">
+          <el-tab-pane label="基本信息" name="0">
+            <el-form-item label="商品名称" prop="goods_name">
+              <el-input v-model="addForm.goods_name"></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格" prop="goods_price">
+              <el-input v-model="addForm.goods_price" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品重量" prop="goods_weight">
+              <el-input v-model="addForm.goods_weight" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品数量" prop="goods_number">
+              <el-input v-model="addForm.goods_number" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品分类" prop="goods_cat">
+              <el-cascader expand-trigger="hover" :options="catelist" :props="cateProps" v-model="addForm.goods_cat" @change="handleChange">
+              </el-cascader>
+            </el-form-item>
+          </el-tab-pane>
+          <el-tab-pane label="商品参数" name="1">
+            <!-- 渲染表单的Item项 -->
+            <el-form-item :label="item.attr_name" v-for="item in manyTableData" :key="item.attr_id">
+              <!-- 复选框组 -->
+              <el-checkbox-group v-model="item.attr_vals">
+                <el-checkbox :label="cb" v-for="(cb, i) in item.attr_vals" :key="i" border></el-checkbox>
+              </el-checkbox-group>
+            </el-form-item>
+          </el-tab-pane>
+          <el-tab-pane label="商品属性" name="2">
+            <el-form-item :label="item.attr_name" v-for="item in onlyTableData" :key="item.attr_id">
+              <el-input v-model="item.attr_vals"></el-input>
+            </el-form-item>
+          </el-tab-pane>
+          <el-tab-pane label="商品图片" name="3">
+            <!-- action 表示图片要上传到的后台API地址 -->
+            <el-upload :action="uploadURL" :on-preview="handlePreview" :on-remove="handleRemove" list-type="picture" :headers="headerObj" :on-success="handleSuccess">
+              <el-button size="small" type="primary">点击上传</el-button>
+            </el-upload>
+          </el-tab-pane>
+          <el-tab-pane label="商品内容" name="4">
+            <!-- 富文本编辑器组件 -->
+            <quill-editor v-model="addForm.goods_introduce"></quill-editor>
+            <!-- 添加商品的按钮 -->
+            <el-button type="primary" class="btnAdd" @click="add">添加商品</el-button>
+          </el-tab-pane>
+        </el-tabs>
+      </el-form>
 
-      </el-card>
+    </el-card>
 
-      <!-- 图片预览 -->
-      <el-dialog title="图片预览" :visible.sync="previewVisible" width="50%">
-        <img :src="previewPath" alt="" class="previewImg">
-      </el-dialog>
-    </div>
-  </template>
+    <!-- 图片预览 -->
+    <el-dialog title="图片预览" :visible.sync="previewVisible" width="50%">
+      <img :src="previewPath" alt="" class="previewImg">
+    </el-dialog>
+  </div>
+</template>
 
 <script>
 import _ from 'lodash'
@@ -132,7 +132,7 @@ export default {
       // 静态属性列表数据
       onlyTableData: [],
       // 上传图片的URL地址
-      uploadURL: 'http://zzhaofengg:8888/api/private/v1/upload',
+      uploadURL: 'http://1.117.80.235:8888/api/private/v1/upload',
       // 图片上传组件的headers请求头对象
       headerObj: {
         Authorization: window.sessionStorage.getItem('token')
@@ -173,14 +173,14 @@ export default {
       }
     },
     async tabClicked () {
-      console.log(this.activeIndex)
+      // console.log(this.activeIndex)
       // 证明访问的是动态参数面板
       if (this.activeIndex === '1') {
         const { data: res } = await this.$http.get(
-            `categories/${this.cateId}/attributes`,
-            {
-              params: { sel: 'many' }
-            }
+          `categories/${this.cateId}/attributes`,
+          {
+            params: { sel: 'many' }
+          }
         )
 
         if (res.meta.status !== 200) {
@@ -190,15 +190,15 @@ export default {
         console.log(res.data)
         res.data.forEach(item => {
           item.attr_vals =
-              item.attr_vals.length === 0 ? [] : item.attr_vals.split(' ')
+            item.attr_vals.length === 0 ? [] : item.attr_vals.split(' ')
         })
         this.manyTableData = res.data
       } else if (this.activeIndex === '2') {
         const { data: res } = await this.$http.get(
-            `categories/${this.cateId}/attributes`,
-            {
-              params: { sel: 'only' }
-            }
+          `categories/${this.cateId}/attributes`,
+          {
+            params: { sel: 'only' }
+          }
         )
 
         if (res.meta.status !== 200) {
@@ -212,7 +212,7 @@ export default {
     // 处理图片预览效果
     handlePreview (file) {
       console.log(file)
-      this.previewPath = file.response.data.url
+      this.previewPath = 'http://1.117.80.235:8888/' + file.response.data.tmp_path
       this.previewVisible = true
     },
     // 处理移除图片的操作
@@ -281,21 +281,20 @@ export default {
       }
       return null
     }
-
   }
 }
 </script>
 
-  <style lang="less" scoped>
-  .el-checkbox {
-    margin: 0 10px 0 0 !important;
-  }
+<style lang="less" scoped>
+.el-checkbox {
+  margin: 0 10px 0 0 !important;
+}
 
-  .previewImg {
-    width: 100%;
-  }
+.previewImg {
+  width: 100%;
+}
 
-  .btnAdd {
-    margin-top: 15px;
-  }
-  </style>
+.btnAdd {
+  margin-top: 15px;
+}
+</style>
