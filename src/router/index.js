ac4f3be457ddc79dbccff6b8d3e404b640ba3b21
zhaofeng <zhaofeng@kuaishou.com>
2021-07-06 14:45:24 +0800

    parans done

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Users from '../components/user/Users.vue'
 import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
 import Cate from '../components/goods/Cate.vue'
+import Params from '../components/goods/Params.vue'
 
 Vue.use(VueRouter)
 const routes = [{
@@ -34,7 +35,11 @@ const routes = [{
     path: '/roles',
     component: Roles
   }, {
-    path: '/categories', component: Cate
+    path: '/categories',
+    component: Cate
+  }, {
+    path: '/params',
+    component: Params
   }]
 }
 ]
