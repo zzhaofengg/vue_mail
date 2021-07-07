4d59d388801f9ec5559eb7b971185521eb202dce
zhaofeng <zhaofeng@kuaishou.com>
2021-07-07 17:25:37 +0800

    done

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
+// 项目发布阶段使用的插件
+const prodPlugins = []
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push('transform-remove-console')
+}
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
@@ -9,6 +14,9 @@ module.exports = {
         libraryName: 'element-ui',
         styleLibraryName: 'theme-chalk'
       }
-    ]
+    ],
+    // 发布产品时候的插件数组
+    ...prodPlugins,
+    '@babel/plugin-syntax-dynamic-import'
   ]
 }
--- src/main-prod.js
+++ src/main-prod.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './plugins/element.js'
+// 导入全局样式表
 import './assets/css/global.css'
+// 导入字体图标
 import './assets/fonts/iconfont.css'
 import axios from 'axios'
 
@@ -10,14 +11,21 @@ import TreeTable from 'vue-table-with-tree-grid'
 // 导入富文本编辑器
 import VueQuillEditor from 'vue-quill-editor'
 // require styles 导入富文本编辑器对应的样式
-import 'quill/dist/quill.core.css'
-import 'quill/dist/quill.snow.css'
-import 'quill/dist/quill.bubble.css'
+
+import NProgress from 'nprogress'
 
 // 配置请求根路径
 axios.defaults.baseURL = 'http://zzhaofengg.asia:8888/api/private/v1/'
+// 在request中使用进度条
 axios.interceptors.request.use(config => {
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+
+// 在request拦截器中，隐藏进度条
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 Vue.prototype.$http = axios
